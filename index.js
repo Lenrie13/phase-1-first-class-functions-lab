e6fdfc7a86c27fd182395c5bfabc6a8d197b025f
Lenrie13 <lenrie.ogutu13@gmail.com>
2024-07-10 11:22:27 +0300

    update firt-class functions lab 2

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+const scuberDrivers = ["Antonia", "Nuru", "Amari", "Mo"];
+
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+}
+
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(int) {
+    return function fareMultiplier(fare) {
+        return fare * int;
+    };
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(scuberDrivers, returnFirstTwoDrivers) {
+    return returnFirstTwoDrivers(scuberDrivers);
+}
